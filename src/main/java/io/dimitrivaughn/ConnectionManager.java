8eb9a7303aa134197cd5d912fccbbe40aa4e58eb
Dimitri Vaughn <dimitrivaughn@Zipcoder27s-MacBook-Pro.local>
2016-05-19 12:42:06 -0400

    created initial classes

---
+++ src/main/java/io/dimitrivaughn/ConnectionManager.java
@@ -0,0 +1,13 @@
+package io.dimitrivaughn;
+
+/**
+ * Created by dimitrivaughn on 5/19/16.
+ */
+public class ConnectionManager {
+
+    public ConnectionManager(){}
+
+    public class ManagedConnection{
+        public ManagedConnection(){}
+    }
+}
---
+++ src/test/java/io/dimitrivaughn/ConnectionManagerSpec.java
@@ -0,0 +1,7 @@
+package io.dimitrivaughn;
+
+/**
+ * Created by dimitrivaughn on 5/19/16.
+ */
+public class ConnectionManagerSpec {
+}
